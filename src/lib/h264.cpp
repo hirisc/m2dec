bdba73b426b62dab6629a75cdc7c76da3627649a
MINEGISHI Takayuki <hirisc@gmail.com>
2011-02-05 18:30:08 +0900

    Source code cleanups.

--- src/lib/h264.cpp
+++ src/lib/h264.cpp
@@ -7212,30 +7212,32 @@ static inline int cabac_decode_decision(h264d_cabac_t *cb, dec_bits *st, int ctx
 		{6, 8, 9, 11}, {6, 7, 9, 10},
 		{6, 7, 8, 9}, {2, 2, 2, 2}
 	};
-	static const int8_t state_trans0[64] = {
-		1, 2, 3, 4, 5, 6, 7, 8,
-		9, 10, 11, 12, 13, 14, 15, 16,
-		17, 18, 19, 20, 21, 22, 23, 24,
-		25, 26, 27, 28, 29, 30, 31, 32,
-		33, 34, 35, 36, 37, 38, 39, 40,
-		41, 42, 43, 44, 45, 46, 47, 48,
-		49, 50, 51, 52, 53, 54, 55, 56,
-		57, 58, 59, 60, 61, 62, 62, 63
+	static const int8_t state_trans[2][64] = {
+		{
+			1, 2, 3, 4, 5, 6, 7, 8,
+			9, 10, 11, 12, 13, 14, 15, 16,
+			17, 18, 19, 20, 21, 22, 23, 24,
+			25, 26, 27, 28, 29, 30, 31, 32,
+			33, 34, 35, 36, 37, 38, 39, 40,
+			41, 42, 43, 44, 45, 46, 47, 48,
+			49, 50, 51, 52, 53, 54, 55, 56,
+			57, 58, 59, 60, 61, 62, 62, 63
+		},
+		{
+			0, 0, 1, 2, 2, 4, 4, 5,
+			6, 7, 8, 9, 9, 11, 11, 12,
+			13, 13, 15, 15, 16, 16, 18, 18,
+			19, 19, 21, 21, 22, 22, 23, 24,
+			24, 25, 26, 26, 27, 27, 28, 29,
+			29, 30, 30, 30, 31, 32, 32, 33,
+			33, 33, 34, 34, 35, 35, 35, 36,
+			36, 36, 37, 37, 37, 38, 38, 63
+		}
 	};
-	static const int8_t state_trans1[64] = {
-		0, 0, 1, 2, 2, 4, 4, 5,
-		6, 7, 8, 9, 9, 11, 11, 12,
-		13, 13, 15, 15, 16, 16, 18, 18,
-		19, 19, 21, 21, 22, 22, 23, 24,
-		24, 25, 26, 26, 27, 27, 28, 29,
-		29, 30, 30, 30, 31, 32, 32, 33,
-		33, 33, 34, 34, 35, 35, 35, 36,
-		36, 36, 37, 37, 37, 38, 38, 63
-	};
-
 	int pStateIdx;
 	uint32_t valMPS, binVal;
 	uint32_t range, offset, lps;
+	int is_lps;
 
 	pStateIdx = cb->context[ctxIdx];
 	valMPS = pStateIdx & 1;
@@ -7246,14 +7248,15 @@ static inline int cabac_decode_decision(h264d_cabac_t *cb, dec_bits *st, int ctx
 	range = range - lps;
 	if (offset < range) {
 		binVal = valMPS;
-		pStateIdx = state_trans0[pStateIdx] * 2 + valMPS;
+		is_lps = 0;
 	} else {
 		binVal = valMPS ^ 1;
 		cb->offset = offset = offset - range;
 		range = lps;
 		valMPS = pStateIdx ? valMPS : valMPS ^ 1;
-		pStateIdx = state_trans1[pStateIdx] * 2 + valMPS;
+		is_lps = 1;
 	}
+	pStateIdx = state_trans[is_lps][pStateIdx] * 2 + valMPS;
 	cb->context[ctxIdx] = pStateIdx;
 	cabac_renorm(cb, st, range, offset);
 	return binVal;
