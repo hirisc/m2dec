0b724d5fb119d4c9abadb49a18304ef989f862bc
MINEGISHI Takayuki <hirisc@gmail.com>
2011-11-13 10:18:16 +0900

    Add wrapper batch file for Jenkins to build

---
+++ Project/HEW/make/jenkins.bat
@@ -0,0 +1,10 @@
+mkdir ..\libm2dec\Release
+mkdir ..\libm2dec\Debug
+mkdir ..\h264dec\Release
+mkdir ..\h264dec\Debug
+mkdir ..\m2dec\Release
+mkdir ..\m2dec\Debug
+"C:\Program Files (x86)\Renesas\Hew\hmake.exe" m2dec.mak > buildlog.txt
+find "ERROR" buildlog.txt
+if errorlevel 1 exit /b 0
+exit /b 1
