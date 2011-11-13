5bbf87347a463d43d80d7ad8773d5dbbd04c899d
MINEGISHI Takayuki <hirisc@gmail.com>
2011-11-13 11:57:29 +0900

    Removed redundant code from makefile.

--- Project/HEW/make/jenkins.bat
+++ Project/HEW/make/jenkins.bat
@@ -6,5 +6,4 @@ mkdir ..\m2dec\Release
 mkdir ..\m2dec\Debug
 "C:\Program Files (x86)\Renesas\Hew\hmake.exe" m2dec.mak > buildlog.txt
 find "Make process completed" buildlog.txt
-if errorlevel 1 exit /b 1
-exit /b 0
+exit /b %errorlevel%
