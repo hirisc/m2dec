ecbf02668b31de03d1b0e7a3c8e37a776ad7dedf
Takayuki Minegishi <hirisc@gmail.com>
2014-07-08 17:54:19 +0900

    Add more LUT generators.

--- extras/printarray.rb
+++ extras/printarray.rb
@@ -1,10 +1,13 @@
 class PrintableArray < Array
+  def elem(v)
+    sprintf("0x%02x", v)
+  end
   def dump(line_max, indent)
     print("\t" * indent, "{\n")
     max = self.length - 1
     prefix = "\t" * (indent + 1)
     self.each_with_index do |v, i|
-      e = sprintf("0x%02x", v)
+      e = elem(v)
       mod = i % line_max
       if mod == 0 then
         print(prefix, e, ",")
@@ -18,5 +21,6 @@ class PrintableArray < Array
     end
     print("\n") if (line_max < self.length) && ((self.length % line_max) != 0)
     print("\t" * indent, "}")
+    self
   end
 end
